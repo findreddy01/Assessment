1f2dbe3bacb2dd33def2de7f3b1fb1b110897857
ayyappa <findreddy01@gmail.com>
2020-01-05 19:14:56 -0500

    Update test

--- script.test.js
+++ script.test.js
@@ -1,22 +1,20 @@
-describe('Assessment test spec', function() {
-    // Add a spyOnEvent
+describe('Assessment test', function() {
 
-    const btnContainer = document.getElementsByClassName('btn__container')[0];
-    const msgContainer = document.getElementsByClassName('msg__container')[0];
-    const hello_btn = document.getElementById('hello');
-    const bye_btn = document.getElementById('bye');
-    const count =5;
+    var msgContainer = document.getElementsByClassName('msg__container')[0];
+    var hello_btn = document.getElementById('hello');
+    var bye_btn = document.getElementById('bye');
+    var count = 5;
 
     beforeEach(function() {
         spyEvent = spyOnEvent('.btn__container', 'click');
     });
 
-    it('should add child divs if clicked on one the buttons if divs are not present', function() {
+    it('should add child divs if clicked on hello buttons if divs are not present', function() {
         hello_btn.click();
         expect(msgContainer.children.length).toBe(count);
     });
 
-    it('should add child divs if clicked on one the buttons if divs are not present', function() {
+    it('should add child divs if clicked on goodbye buttons if divs are not present', function() {
         bye_btn.click();
         expect(msgContainer.children.length).toBe(count);
     });
